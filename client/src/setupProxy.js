608d638c0b82d276c3f32d4b6e20251d2fcbd5ec
Stephen <smzech@gmail.com>
2018-11-11 04:11:27 -0800

    Redux and Authentication

--- client/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-// added for proxy support for Create-React-App 2.0+
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy('/', { target: 'http://localhost:5000' }));
-};
